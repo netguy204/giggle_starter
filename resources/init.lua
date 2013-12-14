7261af01cde9035292d1455056baa70fe18ece40
Brian Taylor <el.wubo@gmail.com>
2013-12-13 20:19:26 -0500

    a bit better starting script

--- resources/init.lua
+++ resources/init.lua
@@ -15,13 +15,12 @@ end
 function solid_mesh(points, color)
    local mesh = game:create_object('Mesh')
    for ii = 1,#points,2 do
-      print(string.format('%d %d', points[ii], points[ii+1]))
       mesh:add_point({points[ii], points[ii+1]}, color)
    end
    return mesh
 end
 
-function game_init()
+function init()
    util.install_basic_keymap()
    world:gravity({0,0})
 
@@ -31,8 +30,11 @@ function game_init()
    local obj = world:create_go()
    local screen_rect = Rect(camera:viewport())
    obj:pos({screen_rect:width()/2, screen_rect:height()/2})
-   --obj:add_component('CTestDisplay', {w=128,h=128})
 
-   local mesh = solid_mesh({100, -100,  0, 100,  -100, -100}, {1,1,0,1})
+   local mesh = solid_mesh({100, -100,  0, 100,  -100, -100}, {1,0,1,1})
    obj:add_component('CMesh', {mesh=mesh, layer=constant.PLAYER})
+end
+
+function game_init()
+   util.protect(init)()
 end
